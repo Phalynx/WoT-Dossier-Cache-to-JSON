cbbfc879643f7181e290d2784450ee50cd0332fc
Marius Czyz <marius.czyz@gmail.com>
2013-06-19 14:03:33 +0200

    Complete resync

---
+++ compile.cmd
@@ -0,0 +1,4 @@
+@echo off
+SET pythonexe=d:\Python27\python.exe
+@%pythonexe% .\compile.py
+pause
---
+++ compile.py
@@ -0,0 +1,6 @@
+def main():
+	import py_compile
+	py_compile.compile('wotdc2j.py')
+
+if __name__ == '__main__':
+	main()
