1a5bd9c352d59e627d689fc0248dd1236b0e8ce5
vBm <the.vbm@gmail.com>
2014-07-10 16:55:59 +0200

    Cosmetics:  - Whitespace cleanup  - Newline at the end of file

--- SafeUnpickler.py
+++ SafeUnpickler.py
@@ -13,13 +13,13 @@ class SafeUnpickler(object):
 	def find_class(cls, module, name):
 		if not module in cls.PICKLE_SAFE:
 			raise cPickle.UnpicklingError('Attempting to unpickle unsafe module %s' % module)
-		
+
 		__import__(module)
 		mod = sys.modules[module]
-			
+
 		if not name in cls.PICKLE_SAFE[module]:
 			raise cPickle.UnpicklingError('Attempting to unpickle unsafe class %s' % name)
-			
+
 		klass = getattr(mod, name)
 		return klass
 
@@ -31,16 +31,16 @@ class SafeUnpickler(object):
 			return safeUnpickler.load()
 		except Exception, e:
 			raise cPickle.UnpicklingError('Unpickler Error')
-			
+
 	@classmethod
 	def load(cls, pickle_file):
 		try:
 			safeUnpickler = cPickle.Unpickler(pickle_file)
 			safeUnpickler.find_global = cls.find_class
 			return safeUnpickler.load()
-		
+
 		except EOFError, er:
 			raise cPickle.UnpicklingError('Unpickler EOF Error')
-		
+
 		except Exception, e:
 			raise cPickle.UnpicklingError('Unpickler Error')
--- wotdc2j.py
+++ wotdc2j.py
@@ -3,7 +3,7 @@
 # Initial version by Phalynx www.vbaddict.net/wot #
 ###################################################
 import struct, json, time, sys, os
-	
+
 def usage():
 	print str(sys.argv[0]) + " dossierfilename.dat [options]"
 	print 'Options:'
@@ -14,22 +14,22 @@ def usage():
 
 
 def main():
-	
+
 	import struct, json, time, sys, os, shutil, datetime, base64
 
 	parserversion = "0.9.1.0"
-	
+
 	global rawdata, tupledata, data, structures, numoffrags
 	global filename_source, filename_target
 	global option_server, option_format, option_tanks
-	
+
 	filename_source = ""
 	option_raw = 0
 	option_format = 0
 	option_server = 0
 	option_frags = 1
 	option_tanks = 0
-	
+
 	for argument in sys.argv:
 		#print "--+++++ " + str(argument)
 		if argument == "-s":
@@ -49,17 +49,17 @@ def main():
 			#print '-- TANK info will be included'
 
 	filename_source = sys.argv[1]
-	
+
 	if filename_source == "":
 		usage()
 		sys.exit(2)
-		
+
 	printmessage('############################################')
 	printmessage('###### WoTDC2J ' + parserversion)
-	
+
 
 	printmessage('Processing ' + filename_source)
-	
+
 
 	if not os.path.exists(filename_source) or not os.path.isfile(filename_source) or not os.access(filename_source, os.R_OK):
 		catch_fatal('Dossier file does not exists')
@@ -68,7 +68,7 @@ def main():
 	if os.path.getsize(filename_source) == 0:
 		catch_fatal('Dossier file size is zero')
 		sys.exit(1)
-		
+
 	filename_target = os.path.splitext(filename_source)[0]
 	filename_target = filename_target + '.json'
 
@@ -78,7 +78,7 @@ def main():
 		except:
 			catch_fatal('Cannot remove target file ' + filename_target)
 
-			
+
 	cachefile = open(filename_source, 'rb')
 
 	try:
@@ -91,19 +91,19 @@ def main():
 		exitwitherror('Dossier cannot be read (dossierCache does not exist)')
 
 	printmessage("Dossier version " + str(dossierversion))
-	
+
 	tankitems = [(k, v) for k, v in dossierCache.items()]
 
 	dossier = dict()
-		
+
 	dossierheader = dict()
 	dossierheader['dossierversion'] = str(dossierversion)
 	dossierheader['parser'] = 'http://www.vbaddict.net'
 	dossierheader['parserversion'] = parserversion
 	dossierheader['tankcount'] = len(tankitems)
-	
 
-	
+
+
 	base32name = "?;?"
 	if option_server == 0:
 		try:
@@ -115,8 +115,8 @@ def main():
 
 	dossierheader['server'] = base32name.split(';', 1)[0];
 	dossierheader['username'] = base32name.split(';', 1)[1];
-	
-	
+
+
 	if option_server == 0:
 		dossierheader['date'] = time.mktime(time.localtime())
 
@@ -141,11 +141,11 @@ def main():
 
 	min_supported = 10
 	max_supported = 81
-		
-		
+
+
 	tanks = dict()
 	tanks_v2 = dict()
-	
+
 	battleCount_15 = 0
 	battleCount_7 = 0
 	battleCount_historical = 0
@@ -153,9 +153,9 @@ def main():
 	battleCount_clan = 0
 	battleCount_fortBattles = 0
 	battleCount_fortSorties = 0
-	
+
 	for tankitem in tankitems:
-		
+
 		if len(tankitem) < 2:
 			printmessage('Invalid tankdata')
 			continue
@@ -163,18 +163,18 @@ def main():
 		if len(tankitem[0]) < 2:
 			printmessage('Invalid tankdata')
 			continue
-			
+
 		rawdata = dict()
 		data = tankitem[1][1]
 		tankstruct = str(len(data)) + 'B'
 		tupledata = struct.unpack(tankstruct, data)
 		tankversion = getdata("tankversion", 0, 1)
-		
+
 		if tankversion < min_supported or tankversion > max_supported:
 				try:
 					write_to_log(get_tank_data(tanksdata, countryid, tankid, "title") + ", unsupported tankversion " + str(tankversion))
 					printmessage('unsupported tankversion')
-					continue				
+					continue
 				except Exception, e:
 					printmessage('unsupported tankversion' + e.message)
 					continue
@@ -182,14 +182,14 @@ def main():
 		if not isinstance(tankitem[0][1], (int)):
 			printmessage('Invalid tankdata')
 			continue
-	
+
 		try:
 			tankid = tankitem[0][1] >> 8 & 65535
 		except Exception, e:
 			exitwitherror('cannot get tankid ' + e.message)
 			continue
 
-			
+
 		try:
 			countryid = tankitem[0][1] >> 4 & 15
 		except Exception, e:
@@ -199,10 +199,10 @@ def main():
 		#if tankid==234 and countryid==1:
 		#	continue
 
-		
+
 		for m in xrange(0,len(tupledata)):
 			rawdata[m] = tupledata[m]
-		
+
 		if len(tupledata) == 0:
 			continue
 
@@ -214,11 +214,11 @@ def main():
 		fragslist = []
 		if tankversion >= 65:
 			tank_v2 = dict()
-			
-			
+
+
 			if tankversion == 65:
 				blocks = ('a15x15', 'a15x15_2', 'clan', 'clan2', 'company', 'company2', 'a7x7', 'achievements', 'frags', 'total', 'max15x15', 'max7x7')
-				
+
 			if tankversion == 69:
 				blocks = ('a15x15', 'a15x15_2', 'clan', 'clan2', 'company', 'company2', 'a7x7', 'achievements', 'frags', 'total', 'max15x15', 'max7x7', 'playerInscriptions', 'playerEmblems', 'camouflages', 'compensation', 'achievements7x7')
 
@@ -228,7 +228,7 @@ def main():
 			if tankversion == 81:
 				blocks = ('a15x15', 'a15x15_2', 'clan', 'clan2', 'company', 'company2', 'a7x7', 'achievements', 'frags', 'total', 'max15x15', 'max7x7', 'playerInscriptions', 'playerEmblems', 'camouflages', 'compensation', 'achievements7x7', 'historical', 'maxHistorical', 'historicalAchievements', 'fortBattles', 'maxFort', 'fortSorties', 'maxSorties', 'fortAchievements')
 
-				
+
 			blockcount = len(list(blocks))+1
 			#print blockcount
 			newbaseoffset = (blockcount * 2)
@@ -241,31 +241,31 @@ def main():
 			numoffrags_historical = 0
 			numoffrags_fortBattles = 0
 			numoffrags_fortSorties = 0
-			
+
 			for blockname in blocks:
-				
+
 
 				if blocksizes[blocknumber] > 0:
 					if blockname == 'frags':
 							fmt = '<' + 'IH' * (blocksizes[blocknumber]/6)
 							fragsdata = struct.unpack_from(fmt, data, newbaseoffset)
-							
+
 						 	for x in range(0, blocksizes[blocknumber]):
 						 		rawdata[newbaseoffset+x] = str(tupledata[newbaseoffset+x]) + " / Frags; "
 
 							index = 0
 							for i in xrange((blocksizes[blocknumber]/6)):
 								compDescr, amount = (fragsdata[index], fragsdata[index + 1])
-								numoffrags_list += amount	
+								numoffrags_list += amount
 								frag_countryid, frag_tankid, frag_tanktitle = get_tank_details(compDescr, tanksdata)
 								tankfrag = [frag_countryid, frag_tankid, amount, frag_tanktitle]
 								fragslist.append(tankfrag)
 								index += 2
-						
+
 							newbaseoffset += blocksizes[blocknumber]
 							if option_frags == 1:
 								tank_v2['fragslist'] = fragslist
-						
+
 					else:
 						oldbaseoffset = newbaseoffset
 						structureddata = getstructureddata(blockname, tankversion, newbaseoffset)
@@ -274,55 +274,55 @@ def main():
 						tank_v2[blockname] = structureddata
 
 				blocknumber +=1
-		
+
 			if contains_block('company', tank_v2):
 				if 'battlesCount' in tank_v2['company']:
 					battleCount_company += tank_v2['company']['battlesCount']
-			
+
 			if contains_block('clan', tank_v2):
 				if 'battlesCount' in tank_v2['clan']:
 					battleCount_company += tank_v2['clan']['battlesCount']
 
 			if contains_block('a15x15', tank_v2):
-				
+
 				if 'battlesCount' in tank_v2['a15x15']:
 					battleCount_15 += tank_v2['a15x15']['battlesCount']
-					
+
 				if 'frags' in tank_v2['a15x15']:
 					numoffrags_a15x15 = int(tank_v2['a15x15']['frags'])
 
 			if contains_block('a7x7', tank_v2):
-				
+
 				if 'battlesCount' in tank_v2['a7x7']:
 					battleCount_7 += tank_v2['a7x7']['battlesCount']
-				
+
 				if 'frags' in tank_v2['a7x7']:
 					numoffrags_a7x7 = int(tank_v2['a7x7']['frags'])
-			
+
 			if contains_block('historical', tank_v2):
-				
+
 				if 'battlesCount' in tank_v2['historical']:
 					battleCount_historical += tank_v2['historical']['battlesCount']
-				
+
 				if 'frags' in tank_v2['historical']:
 					numoffrags_historical = int(tank_v2['historical']['frags'])
 
 			if contains_block('fortBattles', tank_v2):
-				
+
 				if 'battlesCount' in tank_v2['fortBattles']:
 					battleCount_fortBattles += tank_v2['fortBattles']['battlesCount']
-				
+
 				if 'frags' in tank_v2['fortBattles']:
 					numoffrags_fortBattles = int(tank_v2['fortBattles']['frags'])
-					
+
 			if contains_block('fortSorties', tank_v2):
-				
+
 				if 'battlesCount' in tank_v2['fortSorties']:
 					battleCount_fortSorties += tank_v2['fortSorties']['battlesCount']
-				
+
 				if 'frags' in tank_v2['fortSorties']:
 					numoffrags_fortSorties = int(tank_v2['fortSorties']['frags'])
-					
+
 			if option_frags == 1:
 
 				try:
@@ -330,9 +330,9 @@ def main():
 						printmessage('Wrong number of frags. ' + str(numoffrags_list) + ' = ' + str(numoffrags_a15x15) + ' + ' + str(numoffrags_a7x7) + ' + ' + str(numoffrags_historical) + ' + ' + str(numoffrags_fortBattles) + ' + ' + str(numoffrags_fortSorties))
 				except Exception, e:
 						write_to_log('Error processing frags: ' + e.message)
-		
-			
-				
+
+
+
 			tank_v2['common'] = {"countryid": countryid,
 				"tankid": tankid,
 				"tanktitle": tanktitle,
@@ -358,44 +358,44 @@ def main():
 				"has_company": contains_block("company", tank_v2),
 				"has_fort": contains_block("fortBattles", tank_v2),
 				"has_sortie": contains_block("fortSorties", tank_v2)
-				
-			
-				
-				
+
+
+
+
 			}
-			
+
 			if option_raw == 1:
 				tank_v2['rawdata'] = rawdata
 
 			tanks_v2[tanktitle] = tank_v2
-			
-			
+
+
 		if tankversion < 65:
 			if tankversion >= 20:
 				company = getstructureddata("company", tankversion, 0)
 				battleCount_company += company['battlesCount']
 				clan = getstructureddata("clan", tankversion, 0)
 				battleCount_clan += clan['battlesCount']
-			
+
 			numoffrags = 0
-	
+
 			structure = getstructureddata("structure", tankversion, 0)
 
 
-			
+
 			if 'fragspos' not in structure:
 				write_to_log('tankversion ' + str(tankversion) + ' not in JSON')
 				continue
-			
+
 			if option_frags == 1 and tankversion >= 17:
 				fragslist = getdata_fragslist(tankversion, tanksdata, structure['fragspos'])
-	
+
 			tankdata = getstructureddata("tankdata", tankversion, 0)
 			battleCount_15 += tankdata['battlesCount']
-	
+
 			if not "creationTime" in tankdata:
 				tankdata['creationTime'] = 1356998400
-	
+
 			common = {"countryid": countryid,
 				"tankid": tankid,
 				"tanktitle": tanktitle,
@@ -413,31 +413,31 @@ def main():
 				"frags": tankdata['frags'],
 				"frags_compare": numoffrags
 			}
-	
+
 			if option_frags == 1 and tankversion >= 17:
 				try:
 					if tankdata['frags'] <> numoffrags:
 						printmessage('Wrong number of frags!')
 				except Exception, e:
 						write_to_log('Error processing frags: ' + e.message)
-	
+
 			series = getstructureddata("series", tankversion, 0)
-	
+
 			special = getstructureddata("special", tankversion, 0)
-	
+
 			battle = getstructureddata("battle", tankversion, 0)
-	
+
 			major = getstructureddata("major", tankversion, 0)
-	
+
 			epic = getstructureddata("epic", tankversion, 0)
-	
-	
-	
+
+
+
 			tank = dict()
-			
+
 			tank['tankdata'] = tankdata
 			tank['common'] = common
-	
+
 			if tankversion >= 20:
 				tank['series'] = series
 				tank['battle'] = battle
@@ -446,18 +446,18 @@ def main():
 				tank['major'] = major
 				tank['clan'] = clan
 				tank['company'] = company
-				
+
 			if option_frags == 1:
 				tank['kills'] = fragslist
-			
+
 			if option_raw == 1:
 				tank['rawdata'] = rawdata
-			
+
 			tanks[tanktitle] = tank
 			#tanks = sorted(tanks.values())
 
-	
-	dossierheader['battleCount_15'] = battleCount_15	
+
+	dossierheader['battleCount_15'] = battleCount_15
 	dossierheader['battleCount_7'] = battleCount_7
 	dossierheader['battleCount_historical'] = battleCount_historical
 	dossierheader['battleCount_company'] = battleCount_company
@@ -465,7 +465,7 @@ def main():
 
 	dossierheader['result'] = "ok"
 	dossierheader['message'] = "ok"
-	
+
 	dossier['header'] = dossierheader
 	dossier['tanks'] = tanks
 	dossier['tanks_v2'] = tanks_v2
@@ -475,12 +475,12 @@ def main():
 	printmessage('###### Done!')
 	printmessage('')
 	sys.exit(0)
-	
-	
+
+
 def get_current_working_path():
 	#workaround for py2exe
 	import sys, os
-	
+
 	try:
 		if hasattr(sys, "frozen"):
 			return os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding( )))
@@ -492,10 +492,10 @@ def get_current_working_path():
 ############################################################################################################################
 
 def contains_block(blockname, blockdata):
-	
+
 	if blockname in blockdata:
 		return 1
-	
+
 	return 0
 
 
@@ -503,18 +503,18 @@ def get_tank_details(compDescr, tanksdata):
 
 	tankid = compDescr >> 8 & 65535
 	countryid = compDescr >> 4 & 15
-		
+
 	if option_server == 0 or option_tanks == 1:
 		tankname = get_tank_data(tanksdata, countryid, tankid, "title")
 	else:
-		tankname = "-"	
+		tankname = "-"
 
 	return countryid, tankid, tankname
 
 
 def printmessage(message):
 	global option_server
-	
+
 	if option_server == 0:
 		print message
 
@@ -532,12 +532,12 @@ def exitwitherror(message):
 
 def dumpjson(dossier):
 	global option_format, option_server, filename_target
-	
+
 	try:
-		
+
 		if option_server == 0:
 			finalfile = open(filename_target, 'w')
-		
+
 			if option_format == 1:
 				finalfile.write(json.dumps(dossier, sort_keys=True, indent=4))
 			else:
@@ -546,22 +546,22 @@ def dumpjson(dossier):
 			print json.dumps(dossier)
 	except Exception, e:
 		printmessage(e)
-		
+
 
 def catch_fatal(message):
 	global option_server
 	import shutil
-		
+
 	write_to_log("WOTDC2J: " + str(message))
 
 
 def write_to_log(logtext):
 	global option_server
 	import datetime, os
-	
+
 	printmessage(logtext)
 	now = datetime.datetime.now()
-	
+
 	if option_server == 1:
 		try:
 			logFile = open("/var/log/wotdc2j/wotdc2j.log", "a+b")
@@ -569,7 +569,7 @@ def write_to_log(logtext):
 			logFile.close()
 		except:
 			printmessage("Cannot write to wotdc2j.log")
-		
+
 
 def getstructureddata(category, tankversion, baseoffset):
 	global sourcedata, structures
@@ -589,25 +589,25 @@ def getstructureddata(category, tankversion, baseoffset):
 def keepCompatibility(structureddata):
 	# Compatibility with older versions
 	# Some names changed in WoT 0.9.0
-		
+
 	if 'directHits' in structureddata:
 		structureddata['hits'] = structureddata['directHits']
-		
+
 	if 'explosionHits' in structureddata:
 		structureddata['he_hits'] = structureddata['explosionHits']
-		
+
 	if 'piercings' in structureddata:
 		structureddata['pierced'] = structureddata['piercings']
-				
+
 	if 'explosionHitsReceived' in structureddata:
 		structureddata['heHitsReceived'] = structureddata['explosionHitsReceived']
-		
+
 	if 'directHitsReceived' in structureddata:
 		structureddata['shotsReceived'] = structureddata['directHitsReceived']
-		
+
 	if 'noDamageDirectHitsReceived' in structureddata:
 		structureddata['noDamageShotsReceived'] = structureddata['noDamageDirectHitsReceived']
-		
+
 
 	return structureddata
 
@@ -616,12 +616,12 @@ def keepCompatibility(structureddata):
 
 def get_json_data(filename):
 	import json, time, sys, os
-	
+
 
 	current_working_path = get_current_working_path()
 
 	os.chdir(current_working_path)
-	
+
 	if not os.path.exists(filename) or not os.path.isfile(filename) or not os.access(filename, os.R_OK):
 		catch_fatal(filename + " does not exists!")
 		sys.exit(1)
@@ -633,8 +633,8 @@ def get_json_data(filename):
 	except Exception, e:
 		catch_fatal(filename + " cannot be loaded as JSON: " + e.message)
 		sys.exit(1)
-		
-		
+
+
 	file_json.close()
 
 	return file_data
@@ -648,7 +648,7 @@ def get_tank_data(tanksdata, countryid, tankid, dataname):
 			if tankdata['countryid'] == countryid:
 				if tankdata['tankid'] == tankid:
 					return tankdata[dataname]
-	
+
 	if dataname == 'title':
 		return 'unknown_' + str(countryid) + '_' + str(tankid)
 
@@ -678,8 +678,8 @@ def getdata_fragslist(tankversion, tanksdata, offset):
 
 				countryid, tankid, tankname = get_tank_details(compDescr, tanksdata)
 				numoffrags = numoffrags + amount
-				
-				
+
+
 				tankfrag = [countryid, tankid, amount, tankname]
 				fragslist.append(tankfrag)
 
@@ -691,7 +691,7 @@ def getdata(name, startoffset, offsetlength):
 
 	if len(data)<startoffset+offsetlength:
 		return 0
-	
+
 	structformat = 'H'
 
 	if offsetlength==1:
@@ -699,16 +699,16 @@ def getdata(name, startoffset, offsetlength):
 
 	if offsetlength==2:
 		structformat = 'H'
-		
+
 	if offsetlength==4:
 		structformat = 'I'
 
 	value = struct.unpack_from('<' + structformat, data, startoffset)[0]
- 	
+
  	for x in range(0, offsetlength):
  		rawdata[startoffset+x] = str(tupledata[startoffset+x]) + " / " + str(value) +  "; " + name
 
-	
+
  	return value
 
 
